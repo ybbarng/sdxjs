b962d28f36006706426352829c55be53ca4d9fc7
ybbarng <ybbarng@gmail.com>
2025-07-30 23:02:01 +0900

    ChatGPT로 코드 최신화

--- chapter-05/check-existing-files.js
+++ chapter-05/check-existing-files.js
@@ -1,16 +1,18 @@
-import glob from 'glob-promise'
+import { glob } from 'glob'       // ✅ 최신 glob v11
 import path from 'path'
 
 const findNew = async (rootDir, pathHashPairs) => {
+  // hash -> path 매핑
   const hashToPath = pathHashPairs.reduce((obj, [path, hash]) => {
     obj[hash] = path
     return obj
   }, {})
 
   const pattern = `${rootDir}/*.bck`
-  const options = {}
+  const options = {}               // 필요 시 glob 옵션 추가 가능
   const existingFiles = await glob(pattern, options)
 
+  // 존재하는 .bck 파일들의 해시를 제거
   existingFiles.forEach(filename => {
     const stripped = path.basename(filename).replace(/\.bck$/, '')
     delete hashToPath[stripped]
