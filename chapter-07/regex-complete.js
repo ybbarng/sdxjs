e83ec07d40308ea56a332b0368e8a32e925b6d06
ybbarng <ybbarng@gmail.com>
2025-09-08 14:18:52 +0900

    regex-seq를 제거하고 rest를 받는 형태로 테스트 코드 개선

--- chapter-07/regex-complete.js
+++ chapter-07/regex-complete.js
@@ -2,7 +2,6 @@ import Alt from './regex-alt.js'
 import Any from './regex-any.js'
 import End from './regex-end.js'
 import Lit from './regex-lit.js'
-import Seq from './regex-seq.js'
 import Start from './regex-start.js'
 
 const main = () => {
@@ -11,25 +10,25 @@ const main = () => {
     ['b', 'a', false, Lit('b')],
     ['a', 'ab', true, Lit('a')],
     ['b', 'ab', true, Lit('b')],
-    ['ab', 'ab', true, Seq(Lit('a'), Lit('b'))],
-    ['ba', 'ab', false, Seq(Lit('b'), Lit('a'))],
+    ['ab', 'ab', true, Lit('a', Lit('b'))],
+    ['ba', 'ab', false, Lit('b', Lit('a'))],
     ['ab', 'ba', false, Lit('ab')],
-    ['^a', 'ab', true, Seq(Start(), Lit('a'))],
-    ['^b', 'ab', false, Seq(Start(), Lit('b'))],
-    ['a$', 'ab', false, Seq(Lit('a'), End())],
-    ['a$', 'ba', true, Seq(Lit('a'), End())],
-    ['a*', '', true, Any('a')],
-    ['a*', 'baac', true, Any('a')],
-    ['ab*c', 'ac', true, Seq(Lit('a'), Any('b'), Lit('c'))],
-    ['ab*c', 'abc', true, Seq(Lit('a'), Any('b'), Lit('c'))],
-    ['ab*c', 'abbbc', true, Seq(Lit('a'), Any('b'), Lit('c'))],
-    ['ab*c', 'abxc', false, Seq(Lit('a'), Any('b'), Lit('c'))],
+    ['^a', 'ab', true, Start(Lit('a'))],
+    ['^b', 'ab', false, Start(Lit('b'))],
+    ['a$', 'ab', false, Lit('a', End())],
+    ['a$', 'ba', true, Lit('a', End())],
+    ['a*', '', true, Any(Lit('a'))],
+    ['a*', 'baac', true, Any(Lit('a'))],
+    ['ab*c', 'ac', true, Lit('a', Any(Lit('b'), Lit('c')))],
+    ['ab*c', 'abc', true, Lit('a', Any(Lit('b'), Lit('c')))],
+    ['ab*c', 'abbbc', true, Lit('a', Any(Lit('b'), Lit('c')))],
+    ['ab*c', 'abxc', false, Lit('a', Any(Lit('b'), Lit('c')))],
     ['ab|cd', 'xaby', true, Alt(Lit('ab'), Lit('cd'))],
     ['ab|cd', 'acdc', true, Alt(Lit('ab'), Lit('cd'))],
     ['a(b|c)d', 'xabdy', true,
-      Seq(Lit('a'), Alt(Lit('b'), Lit('c')), Lit('d'))],
+      Lit('a', Alt(Lit('b'), Lit('c')), Lit('d'))],
     ['a(b|c)d', 'xabady', false,
-      Seq(Lit('a'), Alt(Lit('b'), Lit('c')), Lit('d'))]
+      Lit('a', Alt(Lit('b'), Lit('c')), Lit('d'))]
   ]
   tests.forEach(([pattern, text, expected, matcher]) => {
     const actual = matcher.match(text)
