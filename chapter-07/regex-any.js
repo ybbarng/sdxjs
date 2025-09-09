fd74eef67923a73db28d1b1429f1b4ef3a3662a3
ybbarng <ybbarng@gmail.com>
2025-09-09 14:22:04 +0900

    exercise 10 과제 수행

--- chapter-07/regex-any.js
+++ chapter-07/regex-any.js
@@ -8,6 +8,8 @@ class RegexAny extends RegexBase {
 
   _match (text, start) {
     const maxPossible = text.length - start
+    // n회 반복에 대한 cache 생성
+    this.cache = this._buildCache(text, start, maxPossible)
     for (let num = maxPossible; num >= 0; num -= 1) {
       const afterMany = this._matchMany(text, start, num)
       if (afterMany !== undefined) {
@@ -17,12 +19,28 @@ class RegexAny extends RegexBase {
     return undefined
   }
 
-  _matchMany (text, start, num) {
-    for (let i = 0; i < num; i += 1) {
+  /**
+   * start에서 시작할 때, 0부터 max회까지 반복한 결과에 대한 캐시를 생성하는 함수
+   *
+   * cache[i]: 주어진 text와 start에 대해 i회 반복했을 때의 결과
+   */
+  _buildCache (text, start, max) {
+    const cache = [start]
+    for (let i = 1; i <= max; i += 1) {
       start = this.child._match(text, start)
+      cache[i] = start;
       if (start === undefined) {
-        return undefined
+        return cache
       }
+    }
+    return cache
+  }
+
+  _matchMany (text, start, num) {
+    // for문을 돌지 않고 cache에서 바로 꺼낸다.
+    start = this.cache[num]
+    if (start === undefined) {
+      return undefined;
     }
     if (this.rest !== null) {
       return this.rest._match(text, start)
